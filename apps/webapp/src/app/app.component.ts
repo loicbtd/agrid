192ca180d3f4546eebb1f6ad1d6f3659d4f0cb0e
loicbertrand-jet <loic.bertrand@jetaviation.com>
2021-12-06 11:54:21 +0100

    setup webapp

---
+++ apps/webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'agrid-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'webapp';
+}
