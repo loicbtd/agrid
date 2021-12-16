072cea76571c246ec8e4bbd242a43070900d86bb
Alexis <Alexis@git>
2021-12-16 09:59:26 +0100

    lint

--- apps/webapp/src/app/modules/support/components/home/home.component.ts
+++ apps/webapp/src/app/modules/support/components/home/home.component.ts
@@ -11,32 +11,27 @@ declare global {
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   ngOnInit(): void {
-
-      const t = 'script';
-      const BASE_URL="https://support.agrid.ml";
-      const g=document.createElement(t),s=document.getElementsByTagName(t)[0];
-      g.src="/assets/js/chatwoot_sdk.js";
-      g.defer = true;
-      g.async = true;
-      s.parentNode && s.parentNode.insertBefore(g,s);
-      g.onload=function(){
-        if(window.chatwootSDK) {
-          window.chatwootSDK.run({
-            websiteToken: 'JDtptbJgfyAYjLJkTUW6eegv',
-            baseUrl: BASE_URL
-          })
-        }
-        
+    const t = 'script';
+    const BASE_URL = 'https://support.agrid.ml';
+    const g = document.createElement(t),
+      s = document.getElementsByTagName(t)[0];
+    g.src = '/assets/js/chatwoot_sdk.js';
+    g.defer = true;
+    g.async = true;
+    s.parentNode && s.parentNode.insertBefore(g, s);
+    g.onload = function () {
+      if (window.chatwootSDK) {
+        window.chatwootSDK.run({
+          websiteToken: 'JDtptbJgfyAYjLJkTUW6eegv',
+          baseUrl: BASE_URL,
+        });
       }
+    };
 
-
-    window.addEventListener('chatwoot:ready', function () {  
-      console.log("antho le bg");
+    window.addEventListener('chatwoot:ready', function () {
+      console.log('antho le bg');
       window.$chatwoot.toggle('open'); // To open widget
-
     });
   }
-
 }
