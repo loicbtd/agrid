f63c7a21d721906dbfcb8a714c3966ced56ff9e7
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-01-19 11:53:29 +0100

    change showcase

--- apps/webapp/src/app/modules/showcase/components/landing-page/landing-page.component.ts
+++ apps/webapp/src/app/modules/showcase/components/landing-page/landing-page.component.ts
@@ -3,6 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss'],
+  styleUrls: [
+    './animate.min.css',
+    './bootstrap.min.css',
+    './jquery-ui.css',
+    './jquery.mCustomScrollbar.min.css',
+    './meanmenu.css',
+    './nice-select.css',
+    './normalize.css',
+    './owl.carousel.min.css',
+    './responsive.css',
+    './slick.css',
+    './style.css',
+  ],
 })
 export class LandingPageComponent {}
