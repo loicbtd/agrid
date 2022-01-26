129e76be9ba716757d4a5fe02c1582bcd534708a
Loïc BERTRAND <loic.bert.marce@gmail.com>
2022-01-26 13:48:01 +0100

    remove register user controller route

--- apps/webservice/src/app/api/controllers/identities.controller.ts
+++ apps/webservice/src/app/api/controllers/identities.controller.ts
@@ -3,7 +3,6 @@ import { SigninRequest } from '@workspace/common/requests';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from '../../domain/services/users.service';
-import { RegisterRequest } from '@workspace/common/requests';
 import { apiRoutes } from '@workspace/common/constants';
 
 @ApiTags(apiRoutes.identities.root)
@@ -15,11 +14,5 @@ export class IdentitiesController {
   @ApiOperation({ summary: 'logs a user' })
   async signin(@Body() command: SigninRequest): Promise<SigninResponse> {
     return await this.usersService.signin(command);
-  }
-
-  @Post(apiRoutes.identities.register)
-  @ApiOperation({ summary: 'registers a user' })
-  async register(@Body() command: RegisterRequest): Promise<void> {
-    await this.usersService.register(command);
   }
 }
