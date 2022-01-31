a97587ffe6a3681444b8d32e52886991c8f545c3
Anthony DUPORT <anthonyduport.ent@gmail.com>
2022-01-31 20:55:37 +0100

    feature fix

--- apps/webapp/src/app/modules/support/components/home/home.component.ts
+++ apps/webapp/src/app/modules/support/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastMessageService } from '../../../../global/services/toast-message.service';
 import { environment } from '../../../../../environments/environment';
+import { Router } from '@angular/router';
 declare global {
   interface Window {
     chatwootSDK?: any;
@@ -13,7 +15,12 @@ declare global {
 export class HomeComponent implements OnInit {
   isChatwootLoaded = false;
 
-  ngOnInit(): void {
+  constructor(
+    private readonly toastService: ToastMessageService,
+    private readonly router: Router
+  ) {}
+
+  async ngOnInit(): Promise<void> {
     const t = 'script';
     const g = document.createElement(t),
       s = document.getElementsByTagName(t)[0];
@@ -30,7 +37,16 @@ export class HomeComponent implements OnInit {
       }
     };
     window.addEventListener('chatwoot:ready', function () {
-      window.$chatwoot.toggle('open'); // To open widget
+      window.$chatwoot.toggle('open');
     });
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    if (!window.$chatwoot.hasLoaded) {
+      this.toastService.showWarning(
+        'La discussion en temps réel ne peut pas être chargée. Vous pouvez nous contacter via le formulaire'
+      );
+      this.router.navigate(['/support/contact']);
+    } else {
+      this.isChatwootLoaded = true;
+    }
   }
 }
