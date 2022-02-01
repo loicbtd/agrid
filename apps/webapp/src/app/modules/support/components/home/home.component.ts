feac18985ec5b7572b86fac569c01f2531a68a72
Alexis <Alexis@git>
2022-02-01 01:23:36 +0100

    integrate chatwoot!!!!

--- apps/webapp/src/app/modules/support/components/home/home.component.ts
+++ apps/webapp/src/app/modules/support/components/home/home.component.ts
@@ -6,14 +6,30 @@ declare global {
   interface Window {
     chatwootSDK?: any;
     $chatwoot?: any;
+    chatwootSettings?: any;
   }
 }
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  styles: [
+    `
+      .footer-section {
+        background: radial-gradient(
+          77.36% 256.97% at 77.36% 57.52%,
+          #4bc714 0%,
+          #e8be28 100%
+        );
+      }
+    `,
+  ],
 })
 export class HomeComponent implements OnInit {
   isChatwootLoaded = false;
+
+  displayMobileMenu = true;
+
+  themeElement: any;
 
   constructor(
     private readonly toastService: ToastMessageService,
@@ -24,11 +40,16 @@ export class HomeComponent implements OnInit {
     const t = 'script';
     const g = document.createElement(t),
       s = document.getElementsByTagName(t)[0];
-    g.src = '/assets/js/chatwoot_sdk.js';
+    g.src=environment.chatwootBaseUrl+"/packs/js/sdk.js";
     g.defer = true;
     g.async = true;
     s.parentNode && s.parentNode.insertBefore(g, s);
     g.onload = function () {
+      window.chatwootSettings = {
+        showPopoutButton: true,
+        type: 'expanded_bubble',
+        launcherTitle: 'Chattez avec nous'
+      }
       if (window.chatwootSDK) {
         window.chatwootSDK.run({
           websiteToken: environment.chatwootEnvironmentToken,
@@ -37,10 +58,12 @@ export class HomeComponent implements OnInit {
       }
     };
     window.addEventListener('chatwoot:ready', function () {
+
       window.$chatwoot.toggle('open');
     });
+
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    if (!window.$chatwoot.hasLoaded) {
+    if (!window.$chatwoot.hasLoaded && !window.chatwootSDK) {
       this.toastService.showWarning(
         'La discussion en temps réel ne peut pas être chargée. Vous pouvez nous contacter via le formulaire'
       );
